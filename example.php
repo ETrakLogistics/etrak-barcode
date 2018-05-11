24223de242678c9c3d09e377b88c642efb80ab86
Richard Barrett <rich@parcelmonkey.com>
2018-05-11 11:28:19 +0100

    Readme

--- example.php
+++ example.php
@@ -1,12 +1,7 @@
 <?PHP
 
-include_once('src/eTrakBarcode.php');
-
+include_once('vendor/autoload.php');
 $b = new \parcelmonkeygroup\eTrakBarcode\eTrakBarcode('ETYCL222200929');
-//$b->overlayLogo(false);
-//$b->overlayRef(false);
-$b->setWidth(200);
-
 echo '<img src="'.$b.'" />';
 
 
